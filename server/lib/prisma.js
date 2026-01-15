3c3530a5797ba5a3bac2a8fad9229da0b0b87557
Harshit Saxena <harshit214saxena@gmail.com>
2026-01-15 20:48:30 +0530

    Update import statement in prisma.js to use TypeScript file extension

--- server/lib/prisma.js
+++ server/lib/prisma.js
@@ -1,5 +1,5 @@
 import "dotenv/config";
-import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaClient } from "../generated/prisma/client.ts";
 import { PrismaNeon } from "@prisma/adapter-neon";
 import { neon, neonConfig } from "@neondatabase/serverless";
 
